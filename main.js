c9f7ae73b181e67ef95b9da5bb77218bb2b60aac
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-05-18 15:38:17 -0500

    Update main.js
    fixed dynamic search & rewrote JS loops

--- main.js
+++ main.js
@@ -1,66 +1,58 @@
-"use strict"
-
+"use strict";
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+	let html = '<tr class="coffee">';
+	html += '<td>' + coffee.id + '</td>';
+	html += '<td>' + coffee.name + '</td>';
+	html += '<td>' + coffee.roast + '</td>';
+	html += '</tr>';
 
-    return html;
+	return html;
 }
 
 function renderCoffees(coffees) {
-    var html = '';
-    for(var i = coffees.length - 1; i >= 0; i--) {
-        html += renderCoffee(coffees[i]);
-    }
-    return html;
+	let html = '';
+	for (let i = 0; i < coffees.length; i++) {
+		html += renderCoffee(coffees[i]);
+	}
+	return html;
 }
 
-function updateCoffees(e) {
-    console.log(e);
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    let searchedCoffee = searchBox.value;
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        } if (coffee.name === searchedCoffee) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+function updateCoffees() {
+	let selectedRoast = roastSelection.value;
+	let searchedCoffee = searchBox.value.toLowerCase();
+	let filteredCoffees = coffees.filter(function (coffee) {
+		if (selectedRoast === 'all') {
+			return coffee.name.toLowerCase().includes(searchedCoffee);
+		} else {
+			return coffee.roast === selectedRoast && coffee.name.toLowerCase().includes(searchedCoffee);
+		}
+	});
+	tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
+let coffees = [
+	{ id: 1, name: 'Light City', roast: 'light' },
+	{ id: 2, name: 'Half City', roast: 'light' },
+	{ id: 3, name: 'Cinnamon', roast: 'light' },
+	{ id: 4, name: 'City', roast: 'medium' },
+	{ id: 5, name: 'American', roast: 'medium' },
+	{ id: 6, name: 'Breakfast', roast: 'medium' },
+	{ id: 7, name: 'High', roast: 'dark' },
+	{ id: 8, name: 'Continental', roast: 'dark' },
+	{ id: 9, name: 'New Orleans', roast: 'dark' },
+	{ id: 10, name: 'European', roast: 'dark' },
+	{ id: 11, name: 'Espresso', roast: 'dark' },
+	{ id: 12, name: 'Viennese', roast: 'dark' },
+	{ id: 13, name: 'Italian', roast: 'dark' },
+	{ id: 14, name: 'French', roast: 'dark' },
 ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+let tbody = document.querySelector('#coffees');
+let roastSelection = document.querySelector('#roast-selection');
+let searchBox = document.querySelector('#coffee-name-add');
 
-let searchBox = document.getElementById('coffee-name-search');
+roastSelection.addEventListener('change', updateCoffees);
+searchBox.addEventListener('keyup', updateCoffees);
 
-
-tbody.innerHTML = renderCoffees(coffees);
-
-searchBox.addEventListener("keyup", updateCoffees);
-submitButton.addEventListener('click', updateCoffees);
+updateCoffees();
