89ef896f8472048eed5f675b76631393be17290e
Xavier G Medina <130258237+xavimedi@users.noreply.github.com>
2023-05-18 16:32:22 -0500

    Update main.js
    fixed eventListener null issue, fixed order of functions, added JS code for AddCoffee, restructured forLoops

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-"use strict";
+"use strict"
+
+let coffees = [
+	{ id: 1, name: 'Light City', roast: 'light' },
+	{ id: 2, name: 'Half City', roast: 'light' },
+	{ id: 3, name: 'Cinnamon', roast: 'light' },
+	{ id: 4, name: 'City', roast: 'medium' },
+	{ id: 5, name: 'American', roast: 'medium' },
+	{ id: 6, name: 'Breakfast', roast: 'medium' },
+	{ id: 7, name: 'High', roast: 'dark' },
+	{ id: 8, name: 'Continental', roast: 'dark' },
+	{ id: 9, name: 'New Orleans', roast: 'dark' },
+	{ id: 10, name: 'European', roast: 'dark' },
+	{ id: 11, name: 'Espresso', roast: 'dark' },
+	{ id: 12, name: 'Viennese', roast: 'dark' },
+	{ id: 13, name: 'Italian', roast: 'dark' },
+	{ id: 14, name: 'French', roast: 'dark' },
+];
+
+let tbody = document.querySelector('#coffees');
+let roastSelection = document.querySelector('#roast-selection');
+let searchBox = document.querySelector('#searchBox');
+let addCoffeeButton = document.querySelector('#add-coffee-button');
+
+if (tbody && roastSelection && searchBox && addCoffeeButton) {
+	roastSelection.addEventListener('change', updateCoffees);
+	searchBox.addEventListener('keyup', updateCoffees);
+	addCoffeeButton.addEventListener('click', addCoffee);
+
+	tbody.innerHTML = renderCoffees(coffees);
+}
 
 function renderCoffee(coffee) {
 	let html = '<tr class="coffee">';
@@ -31,28 +61,24 @@ function updateCoffees() {
 	tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-let coffees = [
-	{ id: 1, name: 'Light City', roast: 'light' },
-	{ id: 2, name: 'Half City', roast: 'light' },
-	{ id: 3, name: 'Cinnamon', roast: 'light' },
-	{ id: 4, name: 'City', roast: 'medium' },
-	{ id: 5, name: 'American', roast: 'medium' },
-	{ id: 6, name: 'Breakfast', roast: 'medium' },
-	{ id: 7, name: 'High', roast: 'dark' },
-	{ id: 8, name: 'Continental', roast: 'dark' },
-	{ id: 9, name: 'New Orleans', roast: 'dark' },
-	{ id: 10, name: 'European', roast: 'dark' },
-	{ id: 11, name: 'Espresso', roast: 'dark' },
-	{ id: 12, name: 'Viennese', roast: 'dark' },
-	{ id: 13, name: 'Italian', roast: 'dark' },
-	{ id: 14, name: 'French', roast: 'dark' },
-];
+function addCoffee(event) {
+	event.preventDefault();
 
-let tbody = document.querySelector('#coffees');
-let roastSelection = document.querySelector('#roast-selection');
-let searchBox = document.querySelector('#coffee-name-add');
+	const coffeeNameInput = document.getElementById('add-coffee');
+	const selectedRoast = document.getElementById('roast-selection-add').value;
+	const newCoffeeName = coffeeNameInput.value.trim();
 
-roastSelection.addEventListener('change', updateCoffees);
-searchBox.addEventListener('keyup', updateCoffees);
+	if (newCoffeeName !== '') {
+		const newCoffee = {
+			id: coffees.length + 1,
+			name: newCoffeeName,
+			roast: selectedRoast
+		};
 
-updateCoffees();
+		coffees.push(newCoffee);
+		tbody.innerHTML = renderCoffees(coffees);
+
+		coffeeNameInput.value = '';
+		document.getElementById('roast-selection-add').value = 'all';
+	}
+}
