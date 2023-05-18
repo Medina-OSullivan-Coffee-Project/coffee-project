f0ea7f66b5bc41e87444b1cc54c0ea9e3b877073
James OSullivan <james.a.osullivan01@gmail.com>
2023-05-18 17:05:04 -0500

    Added some code comments to help explain what is going on.

--- main.js
+++ main.js
@@ -16,12 +16,15 @@ let coffees = [
 	{ id: 13, name: 'Italian', roast: 'dark' },
 	{ id: 14, name: 'French', roast: 'dark' },
 ];
-
+// line 20 selects the tbody element with class of coffees and is used to display the data
 let tbody = document.querySelector('#coffees');
+// Line 22 selects the dropdown menu
 let roastSelection = document.querySelector('#roast-selection');
+// Line 24 selects the element with ID of searchbox and assigns it to the searchbox variable
 let searchBox = document.querySelector('#searchBox');
 let addCoffeeButton = document.querySelector('#add-coffee-button');
 
+// conditional statements checks if the variables are true. If so it adds the event listeners and renders initial coffee data
 if (tbody && roastSelection && searchBox && addCoffeeButton) {
 	roastSelection.addEventListener('change', updateCoffees);
 	searchBox.addEventListener('keyup', updateCoffees);
@@ -29,7 +32,7 @@ if (tbody && roastSelection && searchBox && addCoffeeButton) {
 
 	tbody.innerHTML = renderCoffees(coffees);
 }
-
+//generates an HTML string representing a table row with the coffee details
 function renderCoffee(coffee) {
 	let html = '<tr class="coffee">';
 	html += '<td>' + coffee.id + '</td>';
@@ -40,6 +43,7 @@ function renderCoffee(coffee) {
 	return html;
 }
 
+// iterates over the array and calls renderCoffee for each coffee object and concatenates the resulting HTML strings
 function renderCoffees(coffees) {
 	let html = '';
 	for (let i = 0; i < coffees.length; i++) {
@@ -47,7 +51,7 @@ function renderCoffees(coffees) {
 	}
 	return html;
 }
-
+// This is called when the roast selection or search box changes. It filters the coffees array based on the selected roast and search query and then updates the HTML of tbody element with the filtered coffees
 function updateCoffees() {
 	let selectedRoast = roastSelection.value;
 	let searchedCoffee = searchBox.value.toLowerCase();
@@ -60,16 +64,16 @@ function updateCoffees() {
 	});
 	tbody.innerHTML = renderCoffees(filteredCoffees);
 }
-
+// addCoffee function is called when the add coffee button is clicked. it prevents the default form submission behavior. It retrieves the input values for the new coffee name and selected roast and creates a new coffee object which is then added to the coffees array and updates the tbody.
 function addCoffee(event) {
 	event.preventDefault();
 
-	const coffeeNameInput = document.getElementById('add-coffee');
-	const selectedRoast = document.getElementById('roast-selection-add').value;
-	const newCoffeeName = coffeeNameInput.value.trim();
+	let coffeeNameInput = document.getElementById('add-coffee');
+	let selectedRoast = document.getElementById('roast-selection-add').value;
+	let newCoffeeName = coffeeNameInput.value.trim();
 
 	if (newCoffeeName !== '') {
-		const newCoffee = {
+		let newCoffee = {
 			id: coffees.length + 1,
 			name: newCoffeeName,
 			roast: selectedRoast
